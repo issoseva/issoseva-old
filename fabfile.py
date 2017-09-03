7ced6a8d6e0928c6960d4eaaf74c6f83d2db8fc2
Noj Vek <noj.vek@mixpanel.com>
2017-09-03 00:13:03 -0700

    Always reset hard

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,12 @@
 from fabric.api import settings, cd, run, local
 
-def deploy():
+def deploy(branch='master'):
     local('git push') # Ensure we have no local commits
 
     with settings(user='root', host_string='new.issoseva.org'):
         with cd('/root/issoseva.org'):
             run("pm2 stop app.json")
-            run("git pull")
+            run("git fetch origin %s" % branch)
+            run("git reset --hard origin/%s" % branch)
             run("yarn install")
             run("pm2 start app.json")
