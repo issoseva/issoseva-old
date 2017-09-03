58045dd1e82e901f1770ab97907c8e4985bfe15b
Noj Vek <noj.vek@mixpanel.com>
2017-09-03 00:11:26 -0700

    log

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(require(`body-parser`).json())
 app.use(api)
 app.use(require(`serve-static`)(`www`))
 
+
+console.log(`start server`)
 // Listen for requests
 const server = app.listen(config.port, () => {
   console.log(`Serving humanity on port ` + server.address().port)
