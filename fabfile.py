c922e796d81cc82dd063ca997e9274016ce198bc
Noj Vek <noj.vek@mixpanel.com>
2017-09-13 22:20:09 -0700

    Use pm2 restart for atomic restarts

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,7 @@ def deploy(branch='master'):
     with settings(user='root', host_string='issoseva.org'):
         with cd('/root/issoseva.org'):
             run('mkdir -p logs')
-            run("pm2 delete app.json")
             run("git fetch origin %s" % branch)
             run("git reset --hard origin/%s" % branch)
             run("yarn install")
-            run("pm2 start app.json")
+            run("pm2 restart app.json")
