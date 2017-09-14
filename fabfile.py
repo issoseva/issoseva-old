07851364bfb785c491959c3aa3e011e8a6bccda0
Noj Vek <noj.vek@mixpanel.com>
2017-09-13 22:20:34 -0700

    quotes on fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ def deploy(branch='master'):
     with settings(user='root', host_string='issoseva.org'):
         with cd('/root/issoseva.org'):
             run('mkdir -p logs')
-            run("git fetch origin %s" % branch)
-            run("git reset --hard origin/%s" % branch)
-            run("yarn install")
-            run("pm2 restart app.json")
+            run('git fetch origin %s' % branch)
+            run('git reset --hard origin/%s' % branch)
+            run('yarn install')
+            run('pm2 restart app.json')
