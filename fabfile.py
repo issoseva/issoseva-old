8ef3e78c315876dd11ddbbd5b47771c5c6a5b211
Noj Vek <noj.vek@mixpanel.com>
2018-03-17 22:34:23 -0700

    git push in local fab

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,7 @@ from fabric.api import settings, cd, run, local, env
 env.use_ssh_config = True
 
 def deploy(branch='master'):
+    local('git push')
     with settings(user='root', host_string='issoseva.org'):
         with cd('/root/issoseva.org'):
             run('mkdir -p logs')
