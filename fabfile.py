41b6a67c248a1c1f92cb10ecc0f29f7c163950d0
Noj Vek <noj.vek@mixpanel.com>
2018-03-19 11:23:37 -0700

    Fix failing build

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,13 @@
+import os
 from fabric.api import settings, cd, run, local, env
 
 env.use_ssh_config = True
 
 def deploy(branch='master'):
-    local('git push')
+    # If not in CI mode push the latest dir
+    if os.environ.get('CI', False):
+        local('git push')
+
     with settings(user='root', host_string='issoseva.org'):
         with cd('/root/issoseva.org'):
             run('mkdir -p logs')
