bd7bf479fe8d10eed5cf98a4c60aaee32798ea5c
Noj Vek <nojvek@gmail.com>
2020-04-18 16:16:30 -0700

    fabric2 upgrade

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,24 @@
 import os
-from fabric.api import settings, cd, run, local, env
 
-env.use_ssh_config = True
+from fabric import task
+from fabric.connection import Connection
 
-def deploy(branch='master'):
+@task
+def deploy(local, branch='master'):
     # If not in CI mode push the latest dir
     if not os.environ.get('CI', False):
-        local('git push')
+        local.run('git push', echo=True)
 
-    with settings(user='root', host_string='issoseva.org'):
-        with cd('/root/issoseva.org'):
-            run('mkdir -p logs')
-            run('git fetch origin %s' % branch)
-            run('git reset --hard origin/%s' % branch)
-            run('yarn install')
-            run('yarn build')
-            run('pm2 restart app.json')
+    with Connection('issoseva.org', user='root') as server:
+        cmd = ' && '.join([
+            'cd /root/issoseva.org',
+            'mkdir -p logs',
+            'git fetch origin %s' % branch,
+            'git reset --hard origin/%s' % branch,
+            'yarn install',
+            'yarn build',
+            'pm2 restart app.json',
+        ])
+        server.run(cmd, echo=True)
+
+
